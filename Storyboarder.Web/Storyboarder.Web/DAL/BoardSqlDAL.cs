b7d4b614aeced9925ec1da77f7ed0be10751fc6c
William Lawrence <williamjlawrence1@gmail.com>
2018-07-16 21:43:50 -0400

    completed Board class and added board DAL (not implemented)

---
+++ Storyboarder.Web/Storyboarder.Web/DAL/BoardSqlDAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storyboarder.Web.DAL
+{
+    public class BoardSqlDAL
+    {
+    }
+}
---
+++ Storyboarder.Web/Storyboarder.Web/Models/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storyboarder.Web.Models
+{
+    public class Board
+    {
+        /// <summary>
+        /// The id of the board in the database.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The title of the board.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The first name of the user that made the outline.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of th user that made the board.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The description of the story in the board.
+        /// </summary>        
+        public string Description { get; set; }
+    }
+}
