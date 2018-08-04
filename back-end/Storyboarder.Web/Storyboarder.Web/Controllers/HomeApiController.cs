4aaf9462b3a05fcd8f41c00e271d9eac6ccc5af1
William Lawrence <williamjlawrence1@gmail.com>
2018-08-04 15:55:51 -0400

    started building API and updated file structure

---
+++ back-end/Storyboarder.Web/Storyboarder.Web/Controllers/HomeApiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Storyboarder.Web.DAL;
+using Storyboarder.Web.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Storyboarder.Web.Controllers
+{
+    [Route("api/boards")]
+    [ApiController]
+    public class HomeApiController : Controller
+    {
+        #region Dependency Injection
+        // Dependency injection configuration
+        private readonly IBoardDAL dal;
+        public HomeApiController(IBoardDAL dal)
+        {
+            this.dal = dal;
+        }
+        #endregion
+
+        /// <summary>
+        /// Gets all the boards from the database.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<Board>> GetAllBoards()
+        {
+            return dal.GetAllBoards().ToList();
+        }
+    }
+}
