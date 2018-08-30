9aaf512622118e2bd4aacd87ebba73856753a552
William Lawrence <williamjlawrence1@gmail.com>
2018-08-30 14:15:39 -0400

    added about and contact views

--- Storyboarder.Web/Storyboarder.Web/Controllers/HomeController.cs
+++ Storyboarder.Web/Storyboarder.Web/Controllers/HomeController.cs
@@ -32,6 +32,20 @@ namespace Storyboarder.Web.Controllers
             return View(boards);
         }
 
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
