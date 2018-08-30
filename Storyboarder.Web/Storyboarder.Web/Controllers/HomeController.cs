c7209b3f6e7858759e19fd212e0dc18340f274a6
William Lawrence <williamjlawrence1@gmail.com>
2018-08-30 14:07:19 -0400

    removed boilerplate from HomeController

--- Storyboarder.Web/Storyboarder.Web/Controllers/HomeController.cs
+++ Storyboarder.Web/Storyboarder.Web/Controllers/HomeController.cs
@@ -32,33 +32,11 @@ namespace Storyboarder.Web.Controllers
             return View(boards);
         }
 
-        #region Microsoft Crap
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        #endregion
     }
 }
